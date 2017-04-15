feb02f5bbc0220334adbda061d2ed94326b93f6d
cody <cls9@pdx.edu>
2017-04-14 17:16:55 -0700

    Pushing to sync with another machine.

--- Pieces.scala
+++ Pieces.scala
@@ -15,7 +15,7 @@ case class White() extends Player {
   def opposite: Player = Black()
 }
 
-sealed class Loc(var x: Int, var y: Int) {
+sealed class Loc(val x: Int, val y: Int) {
 
   override def equals(o: Any): Boolean = {
     o match {
@@ -50,9 +50,11 @@ case class Pawn(p: Player, l: Loc) extends Piece(p,l) {
   def fwd(st: State): State = {
     val pred = PartialFunction(this.l.equals)
     val np = st.pieces.filterNot((x: Piece) => x == this)
+    /*
     System.err.println("Function fwd")
     System.err.println("s.pieces: " + st.pieces)
     System.err.println("np: " + np)
+    */
     this.p match {
       case a: Black => {
         val newp = Pawn(p = this.p, l = new Loc(x = this.l.x-1, y = this.l.y))
@@ -68,7 +70,7 @@ case class Pawn(p: Player, l: Loc) extends Piece(p,l) {
   def capRight(st: State): State = {
     val removeMe = (x: Piece) => x == this
     val removeCap = {
-      (mp: Piece) => mp.getPlayer match {
+      (mp: Piece) => this.getPlayer match {
           case a: Black => mp.getLoc.x == (this.l.x - 1) && mp.getLoc.y == (this.l.y + 1)
           case a: White => mp.getLoc.x == (this.l.x + 1) && mp.getLoc.y == (this.l.y + 1)
         }
@@ -90,7 +92,7 @@ case class Pawn(p: Player, l: Loc) extends Piece(p,l) {
   def capLeft(st: State): State = {
     val removeMe = (x: Piece) => x == this
     val removeCap = {
-      (mp: Piece) => mp.getPlayer match {
+      (mp: Piece) => this.getPlayer match {
         case a: Black => mp.getLoc.x == (this.l.x - 1) && mp.getLoc.y == (this.l.y - 1)
         case a: White => mp.getLoc.x == (this.l.x + 1) && mp.getLoc.y == (this.l.y - 1)
       }
